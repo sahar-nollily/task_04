fd8863a680a1dfdbcca1870b52d8a7b7717f52c7
Sa7ar Nollily <sa7arnollily@Sahar-2.local>
2019-07-23 16:46:43 +0300

    task4_2

--- restaurants/models.py
+++ restaurants/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Restaurant (models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
-    opening_time = models.TimeField(null=True)
-    closing_time = models.TimeField(null=True)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
 
-def __str__(self):
-        return self.title
+    def __str__(self):
+            return self.name
